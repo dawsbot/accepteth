c25b0b06904ed7a62933cafc1cf65774663499d6
Dawson Botsford <dawsonbotsford@gmail.com>
2020-10-21 17:05:33 -0600

    Unicorn homepage setup - configure default eth aswell

--- pages/v0/build-portal.tsx
+++ pages/v0/build-portal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import Select from "react-select";
@@ -276,9 +276,6 @@ const AcceptAny = () => {
               }}
             >
               <TextDisplayWithCopyButton text={buildHref} isAnchor />
-              {/* <a href={buildHref} target="_blank" style={{ color: "black" }}>
-                <code>{buildHref}</code>
-              </a> */}
             </div>
           </>
         )}
