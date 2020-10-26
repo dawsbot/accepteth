e73d8f8c0076b2688329f3edd7cb22de6e4f7461
Dawson Botsford <dawsonbotsford@gmail.com>
2020-10-26 09:31:19 -0600

    SEO homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import Head from "next/head";
+import React from "react";
+
 const Index = () => {
   if (typeof window !== "undefined") {
     window.location.href = "https://accepteth.unicornplatform.com/";
   }
-  return null;
+  return (
+    <Head>
+      <title>AcceptEth</title>
+      <meta name="og:title" content="AcceptEth" />
+    </Head>
+  );
 };
 
 export default Index;
